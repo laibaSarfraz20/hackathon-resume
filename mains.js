11208244f8d12395e320194918d1406595b1eb18
laibaSarfraz20 <154825429+laibaSarfraz20@users.noreply.github.com>
2024-09-08 14:06:59 +0500

    module 5

--- mains.js
+++ mains.js
@@ -11,26 +11,37 @@ var _a;
     var experience = document.getElementById("experience");
     var skills = document.getElementById("skills");
     var achievements = document.getElementById("achievements");
-    if (profile && name && email && contact && address && education && experience && skills && achievements) {
+    var userName = document.getElementById("username");
+    if (profile && name && email && contact && address && education && experience && skills && achievements && userName) {
         var nameElement = name.value;
         var emailElement = email.value;
         var contactElement = contact.value;
         var addressElement = address.value;
-        var achievementsElement = education.value;
+        var achievementsElement = achievements.value;
         var educationElement = education.value;
-        var experienceElement = achievements.value;
+        var experienceElement = experience.value;
         var skillsElement = skills.value;
+        var userNameElement = userName.value;
+        var uniquePath = "resume/".concat(userNameElement.replace(/\s+/g, '_'), "_cv.html");
+        var downloadLink = document.createElement('a');
+        var resumeOutputElement = document.getElementById("resume-output");
         var profilePictureFile = (_a = profile.files) === null || _a === void 0 ? void 0 : _a[0];
         var profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : "";
-        var resumeOutput = "\n        <h2>Resume</h2>\n        ".concat(profilePictureURL ? "img src=\" ".concat(profilePictureURL, "\" alt=\"profile\" class = \"prof\"> ") : "", "\n\n        <p> Name:<span id=\"edit-name\" class=\"editable\"> ").concat(nameElement, " </span></p>\n        <p> Email: <span id=\"edit-email\" class=\"editable\"> ").concat(emailElement, " </span></p>\n        <p> Contact: <span id=\"edit-contact\" class=\"editable\"> ").concat(contactElement, " </span></p>\n        <p> Address: <span id=\"edit-address\" class=\"editable\"> ").concat(addressElement, " </span></p>\n\n        <p> Key Achievements:<p id=\"edit-acheivements\" class=\"editable\"> ").concat(achievementsElement, " </p></p>\n        <p> Education:<p  id=\"edit-education\" class=\"editable\"> ").concat(educationElement, " </p> </p>\n        <p> Experience: <p  id=\"edit-education\" class=\"editable\">").concat(experienceElement, " </p></p>\n        <p> Skills: <p  id=\"edit-education\" class=\"editable\"> ").concat(skillsElement, "</p></p>\n        ");
-        var resumeOutputElement = document.getElementById("resume-output");
+        var resumeOutput = "\n            <h2>Resume</h2>\n            ".concat(profilePictureURL ? "<img src=\"".concat(profilePictureURL, "\" alt=\"profile\" class=\"prof\">") : "No profile picture selected", "\n\n            <p>Name: <span id=\"edit-name\" class=\"editable\">").concat(nameElement, "</span></p>\n            <p>Email: <span id=\"edit-email\" class=\"editable\">").concat(emailElement, "</span></p>\n            <p>Contact: <span id=\"edit-contact\" class=\"editable\">").concat(contactElement, "</span></p>\n            <p>Address: <span id=\"edit-address\" class=\"editable\">").concat(addressElement, "</span></p>\n\n            <p>Key Achievements: <span id=\"edit-achievements\" class=\"editable\">").concat(achievementsElement, "</span></p>\n            <p>Education: <span id=\"edit-education\" class=\"editable\">").concat(educationElement, "</span></p>\n            <p>Experience: <span id=\"edit-experience\" class=\"editable\">").concat(experienceElement, "</span></p>\n            <p>Skills: <span id=\"edit-skills\" class=\"editable\">").concat(skillsElement, "</span></p>\n        ");
         if (resumeOutputElement) {
             resumeOutputElement.innerHTML = resumeOutput;
+            downloadLink.href = "data:text/html;charset=utf-8,".concat(encodeURIComponent(resumeOutput));
+            downloadLink.download = uniquePath;
+            downloadLink.textContent = 'Download your Resume';
+            resumeOutputElement.appendChild(downloadLink);
             makeEditable();
         }
         else {
-            console.error("some elements are missing!");
+            console.error("Some elements are missing!");
         }
+    }
+    else {
+        console.error("Please fill in all the required fields!");
     }
 });
 function makeEditable() {
@@ -40,19 +51,19 @@ function makeEditable() {
             var _a;
             var currentElements = element;
             var currentValue = currentElements.textContent || '';
-            if (currentElements.tagName === "p" || currentElements.tagName === "span") {
-                var input = document.createElement("input");
-                input.type = "text"; // corrected here
-                input.value = currentValue;
-                input.classList.add("editing-input");
-                input.addEventListener("blur", function () {
-                    currentElements.textContent = input.value;
-                    currentElements.style.display = " inline";
-                    input.remove();
+            if (currentElements.tagName === "SPAN") {
+                var input_1 = document.createElement("input");
+                input_1.type = "text";
+                input_1.value = currentValue;
+                input_1.classList.add("editing-input");
+                input_1.addEventListener("blur", function () {
+                    currentElements.textContent = input_1.value;
+                    currentElements.style.display = "inline";
+                    input_1.remove();
                 });
                 currentElements.style.display = "none";
-                (_a = currentElements.parentNode) === null || _a === void 0 ? void 0 : _a.insertBefore(input, currentElements);
-                input.focus();
+                (_a = currentElements.parentNode) === null || _a === void 0 ? void 0 : _a.insertBefore(input_1, currentElements);
+                input_1.focus();
             }
         });
     });
--- mains.ts
+++ mains.ts
@@ -2,76 +2,83 @@
 document.getElementById("form-resume")?.addEventListener("submit", function (event) {
     event?.preventDefault();
 
-       var profile = document.getElementById("profilePicture") as HTMLInputElement;
-       var name = document.getElementById("name") as HTMLInputElement;
-       var email = document.getElementById("email") as HTMLInputElement;
-       var contact = document.getElementById("contact") as HTMLInputElement;
-       var address = document.getElementById("address") as HTMLInputElement;
-       var education = document.getElementById("education") as HTMLInputElement;
-       var experience = document.getElementById("experience") as HTMLInputElement;
-       var skills = document.getElementById("skills") as HTMLInputElement;
-       var achievements = document.getElementById("achievements") as HTMLInputElement;
+    const profile = document.getElementById("profilePicture") as HTMLInputElement;
+    const name = document.getElementById("name") as HTMLInputElement;
+    const email = document.getElementById("email") as HTMLInputElement;
+    const contact = document.getElementById("contact") as HTMLInputElement;
+    const address = document.getElementById("address") as HTMLInputElement;
+    const education = document.getElementById("education") as HTMLInputElement;
+    const experience = document.getElementById("experience") as HTMLInputElement;
+    const skills = document.getElementById("skills") as HTMLInputElement;
+    const achievements = document.getElementById("achievements") as HTMLInputElement;
+    const userName = document.getElementById("username") as HTMLInputElement;
 
-     if ( profile && name && email && contact && address && education && experience && skills && achievements) {
-       
-        var nameElement = name.value;
-        var emailElement = email.value;
-        var contactElement = contact.value;
-        var addressElement = address.value;
-        var achievementsElement = education.value;
-        var educationElement = education.value;
-        var experienceElement = achievements.value;
-        var skillsElement = skills.value;
+    if (profile && name && email && contact && address && education && experience && skills && achievements && userName) {
+        const nameElement = name.value;
+        const emailElement = email.value;
+        const contactElement = contact.value;
+        const addressElement = address.value;
+        const achievementsElement = achievements.value;
+        const educationElement = education.value;
+        const experienceElement = experience.value;
+        const skillsElement = skills.value;
+        const userNameElement = userName.value;
 
+        const uniquePath = `resume/${userNameElement.replace(/\s+/g, '_')}_cv.html`;
 
-         var profilePictureFile = profile.files?.[0]
-         var profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile): "";
- 
+        const downloadLink = document.createElement('a');
+        const resumeOutputElement = document.getElementById("resume-output");
 
+        const profilePictureFile = profile.files?.[0];
+        const profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : "";
 
-        var resumeOutput = `
-        <h2>Resume</h2>
-        ${profilePictureURL ? `img src=" ${profilePictureURL}" alt="profile" class = "prof"> `: ""}
+        const resumeOutput = `
+            <h2>Resume</h2>
+            ${profilePictureURL ? `<img src="${profilePictureURL}" alt="profile" class="prof">` : "No profile picture selected"}
 
-        <p> Name:<span id="edit-name" class="editable"> ${nameElement} </span></p>
-        <p> Email: <span id="edit-email" class="editable"> ${emailElement} </span></p>
-        <p> Contact: <span id="edit-contact" class="editable"> ${contactElement} </span></p>
-        <p> Address: <span id="edit-address" class="editable"> ${addressElement} </span></p>
+            <p>Name: <span id="edit-name" class="editable">${nameElement}</span></p>
+            <p>Email: <span id="edit-email" class="editable">${emailElement}</span></p>
+            <p>Contact: <span id="edit-contact" class="editable">${contactElement}</span></p>
+            <p>Address: <span id="edit-address" class="editable">${addressElement}</span></p>
 
-        <p> Key Achievements:<p id="edit-acheivements" class="editable"> ${achievementsElement} </p></p>
-        <p> Education:<p  id="edit-education" class="editable"> ${educationElement} </p> </p>
-        <p> Experience: <p  id="edit-education" class="editable">${experienceElement} </p></p>
-        <p> Skills: <p  id="edit-education" class="editable"> ${skillsElement}</p></p>
-        `
+            <p>Key Achievements: <span id="edit-achievements" class="editable">${achievementsElement}</span></p>
+            <p>Education: <span id="edit-education" class="editable">${educationElement}</span></p>
+            <p>Experience: <span id="edit-experience" class="editable">${experienceElement}</span></p>
+            <p>Skills: <span id="edit-skills" class="editable">${skillsElement}</span></p>
+        `;
 
-        var resumeOutputElement = document.getElementById("resume-output")
-         if (resumeOutputElement) {
-            resumeOutputElement.innerHTML = resumeOutput
-            makeEditable()
-          } else {
-            console.error("some elements are missing!")
+        if (resumeOutputElement) {
+            resumeOutputElement.innerHTML = resumeOutput;
+            downloadLink.href = `data:text/html;charset=utf-8,${encodeURIComponent(resumeOutput)}`;
+            downloadLink.download = uniquePath;
+            downloadLink.textContent = 'Download your Resume';
+            resumeOutputElement.appendChild(downloadLink);
+
+            makeEditable();
+        } else {
+            console.error("Some elements are missing!");
         }
-
-     }   
-      
+    } else {
+        console.error("Please fill in all the required fields!");
     }
-)
-function makeEditable(){
-    var editableElements = document.querySelectorAll(".editable");
-    editableElements.forEach(element => {
-        element.addEventListener('click', function(){
-            var currentElements = element as HTMLElement;
-            var currentValue = currentElements.textContent || '';
+});
 
-            if(currentElements.tagName === "p" || currentElements.tagName === "span"){
-                var input = document.createElement("input");
-                input.type = "text"; // corrected here
+function makeEditable() {
+    const editableElements = document.querySelectorAll(".editable");
+    editableElements.forEach(element => {
+        element.addEventListener('click', function () {
+            const currentElements = element as HTMLElement;
+            const currentValue = currentElements.textContent || '';
+
+            if (currentElements.tagName === "SPAN") {
+                const input = document.createElement("input");
+                input.type = "text";
                 input.value = currentValue;
                 input.classList.add("editing-input");
 
-                input.addEventListener("blur",function(){
+                input.addEventListener("blur", function () {
                     currentElements.textContent = input.value;
-                    currentElements.style.display =" inline";
+                    currentElements.style.display = "inline";
                     input.remove()
                 })
 
